ab2fa3ddc8696eafac76ecbe09dedf1868941825
jonhpol <j.paulo_os@hotmail.com>
2019-11-17 23:31:50 -0300

    Update .travis.yml

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "com.JP.RPSGame" } // The bundle identifier of your app
+var appleID: String { return "j.paulo_os@hotmail.com" } // Your Apple email address
+
+var itcTeam: String? { return "119351984" } // App Store Connect Team ID
+var teamID: String { return "V9TV25TFN7" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
