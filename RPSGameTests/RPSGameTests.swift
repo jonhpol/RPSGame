a6fae8c4a7f13fdb39d44f1bbf0d5629bf1373a0
jonhpol <j.paulo_os@hotmail.com>
2019-11-17 23:19:29 -0300

    Add tests

---
+++ RPSGameTests/RPSGameTests.swift
@@ -0,0 +1,33 @@
+//
+//  RPSGameTests.swift
+//  RPSGameTests
+//
+//  Created by João Paulo de Oliveira Sabino on 17/11/19.
+//  Copyright © 2019 João Paulo de Oliveira Sabino. All rights reserved.
+//
+
+import XCTest
+
+class RPSGameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
